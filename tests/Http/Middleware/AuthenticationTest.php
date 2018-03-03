16bcc453aa5ae8ac885f83d3e6ff61dba5941831
Tim Rourke <tim@timrourke.com>
2018-03-03 09:34:47 -0600

    Add tests for unauthenticated requests

--- tests/Http/Middleware/AuthenticationTest.php
+++ tests/Http/Middleware/AuthenticationTest.php
@@ -85,6 +85,56 @@ class AuthenticationTest extends TestCase
         );
     }
 
+    public function testProcessWhenAuthorizationHeaderMalformed(): void
+    {
+        $sut = $this->getAuthentication();
+
+        $unauthenticatedRequest = new ServerRequest(
+            'GET',
+            'localhost',
+            [
+                'Authorization' => 'Token ' . $this->getValidTokenString(),
+            ]
+        );
+
+        $handler = new PsrRequestHandlerStub();
+
+        $response = $sut->process(
+            $unauthenticatedRequest,
+            $handler
+        );
+
+        $this->assertEquals(
+            401,
+            $response->getStatusCode()
+        );
+    }
+
+    public function testProcessWhenTokenInvalid(): void
+    {
+        $sut = $this->getAuthentication();
+
+        $unauthenticatedRequest = new ServerRequest(
+            'GET',
+            'localhost',
+            [
+                'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ',
+            ]
+        );
+
+        $handler = new PsrRequestHandlerStub();
+
+        $response = $sut->process(
+            $unauthenticatedRequest,
+            $handler
+        );
+
+        $this->assertEquals(
+            401,
+            $response->getStatusCode()
+        );
+    }
+
     private function getTokenService(): Service
     {
         return ServiceFactory::make(
