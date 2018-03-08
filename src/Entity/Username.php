a9a64b5891b57dfc0f27ba88e5f0d5c64b395032
Tim Rourke <tim@timrourke.com>
2018-03-08 07:35:58 -0600

    Add Username value object

---
+++ src/Entity/Username.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Incognito\Entity;
+
+use Assert\Assertion;
+
+class Username
+{
+    /**
+     * @var string
+     */
+    private const ALLOWABLE_CHARACTERS_REGEX = "/[\p{L}\p{M}\p{S}\p{N}\p{P}]+/u";
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * Constructor.
+     *
+     * @param string $username
+     */
+    public function __construct(string $username)
+    {
+        $this->setUsername($username);
+    }
+
+    /**
+     * @return string
+     */
+    public function username(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * Validate and set the username
+     *
+     * @param string $username
+     * @return void
+     * @throws \Assert\AssertionFailedException
+     */
+    private function setUsername(string $username): void
+    {
+        Assertion::betweenLength(
+            $username,
+            1,
+            128,
+            sprintf(
+                "Invalid username \"%s\": username must be between 1 and 128 characters in length.",
+                $username
+            )
+        );
+
+        Assertion::regex(
+            $username,
+            self::ALLOWABLE_CHARACTERS_REGEX,
+            sprintf(
+                "Invalid username \"%s\": username contains invalid characters.",
+                $username
+            )
+        );
+
+        $this->username = $username;
+    }
+}
