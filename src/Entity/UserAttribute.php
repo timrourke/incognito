11a6cbadf814030fcddb813f6289630295ad7c26
Tim Rourke <tim@timrourke.com>
2018-03-10 06:53:01 -0600

    Add UserAttribute

---
+++ src/Entity/UserAttribute.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Incognito\Entity;
+
+use Assert\Assertion;
+
+class UserAttribute
+{
+    /**
+     * @var string
+     */
+    private const ALLOWABLE_CHARACTERS_REGEX = "/[\p{L}\p{M}\p{S}\p{N}\p{P}]+/u";
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * Constructor.
+     *
+     * @param string $name
+     * @param string $value
+     */
+    public function __construct(string $name, string $value = '')
+    {
+        $this->setName($name);
+        $this->setValue($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function value(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $name
+     * @return \Incognito\Entity\UserAttribute
+     * @throws \Assert\AssertionFailedException
+     */
+    private function setName(string $name): UserAttribute
+    {
+        Assertion::betweenLength(
+            $name,
+            1,
+            32,
+            sprintf(
+                "Invalid name \"%s\": name must be between 1 and 32 characters in length.",
+                $name
+            )
+        );
+
+        Assertion::regex(
+            $name,
+            self::ALLOWABLE_CHARACTERS_REGEX,
+            sprintf(
+                "Invalid name \"%s\": name contains invalid characters.",
+                $name
+            )
+        );
+
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @param string $value
+     * @return UserAttribute
+     * @throws \Assert\AssertionFailedException
+     */
+    private function setValue(string $value): UserAttribute
+    {
+        Assertion::maxLength(
+            $value,
+            2048,
+            sprintf(
+                "Invalid value \"%s\": value must be between 0 and 2048 characters in length.",
+                $value
+            )
+        );
+
+        $this->value = $value;
+
+        return $this;
+    }
+}
