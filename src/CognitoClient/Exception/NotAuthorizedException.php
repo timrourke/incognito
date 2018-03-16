8943c94f29fee189de18df15bd951600f93b1c3e
Tim Rourke <tim@timrourke.com>
2018-03-15 20:02:02 -0500

    Add NotAuthorizedException

---
+++ src/CognitoClient/Exception/NotAuthorizedException.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Incognito\CognitoClient\Exception;
+
+use Aws\Exception\AwsException;
+use Exception;
+
+/**
+ * Class NotAuthorizedException
+ *
+ * Useful when a login attempt is made with an incorrect username or password.
+ *
+ * @package Incognito\CognitoClient\Exception
+ */
+class NotAuthorizedException extends Exception
+{
+    /**
+     * @var int
+     */
+    private const CODE = 401;
+
+    /**
+     * @var string
+     */
+    private const MESSAGE = 'Login failed: Incorrect username or password.';
+
+    /**
+     * NotAuthorizedException constructor.
+     *
+     * @param \Aws\Exception\AwsException|null $previous
+     */
+    public function __construct(AwsException $previous = null)
+    {
+        parent::__construct(self::MESSAGE, self::CODE, $previous);
+    }
+}
