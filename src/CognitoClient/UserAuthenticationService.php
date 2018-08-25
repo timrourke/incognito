cf20b701d35127d0de7d6191ff14b38a71637fdf
Tim Rourke <tim@timrourke.com>
2018-08-25 06:14:01 -0500

    Rough out user password change command

--- src/CognitoClient/UserAuthenticationService.php
+++ src/CognitoClient/UserAuthenticationService.php
@@ -134,6 +134,34 @@ class UserAuthenticationService
     }
 
     /**
+     * Change a User's password
+     *
+     * @param string $accessToken
+     * @param \Incognito\Entity\Password $previousPassword
+     * @param \Incognito\Entity\Password $proposedPassword
+     * @return \Aws\Result
+     */
+    public function changePassword(
+        string $accessToken,
+        Password $previousPassword,
+        Password $proposedPassword
+    ): Result {
+        $result = null;
+
+        try {
+            $result = $this->cognitoClient->changePassword([
+                'AccessToken' => $accessToken,
+                'PreviousPassword' => $previousPassword->password(),
+                'ProposedPassword' => $proposedPassword->password(),
+            ]);
+        } catch(AwsException $e) {
+            $this->handleChangePasswordAwsException($e);
+        }
+
+        return $result;
+    }
+
+    /**
      * @param \Aws\Exception\AwsException $e
      * @throws \Incognito\CognitoClient\Exception\NotAuthorizedException
      * @throws \Incognito\CognitoClient\Exception\UserNotConfirmedException
@@ -157,11 +185,21 @@ class UserAuthenticationService
      * @param \Aws\Exception\AwsException $e
      * @throws \Incognito\CognitoClient\Exception\UsernameExistsException
      */
-    private function handleSignUpUserAwsException(AwsException $e)
+    private function handleSignUpUserAwsException(AwsException $e): void
     {
         switch ($e->getAwsErrorCode()) {
             case 'UsernameExistsException':
                 throw new UsernameExistsException($e);
+            default:
+                throw $e;
+        }
+    }
+
+    private function handleChangePasswordAwsException(AwsException $e): void
+    {
+        switch ($e->getAwsErrorCode()) {
+            case 'InvalidPasswordException':
+                throw new InvalidPasswordException($e);
             default:
                 throw $e;
         }
