a160269ef19c7523ba1b0338d8c9a86a499ad2e1
Tim Rourke <tim@timrourke.com>
2018-08-25 06:32:48 -0500

    Add InvalidPasswordException

---
+++ src/CognitoClient/Exception/InvalidPasswordException.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Incognito\CognitoClient\Exception;
+
+use Aws\Exception\AwsException;
+use Exception;
+
+/**
+ * Class InvalidPasswordException
+ *
+ * Useful when creating or updating a user fails because a password is invalid.
+ *
+ * @package Incognito\CognitoClient\Exception
+ */
+class InvalidPasswordException extends Exception
+{
+    /**
+     * @var int
+     */
+    private const CODE = 422;
+
+    /**
+     * @var string
+     */
+    private const MESSAGE = 'Invalid password.';
+
+    /**
+     * InvalidPasswordException constructor.
+     *
+     * @param \Aws\Exception\AwsException|null $previous
+     */
+    public function __construct(AwsException $previous = null)
+    {
+        parent::__construct(self::MESSAGE, self::CODE, $previous);
+    }
+}
